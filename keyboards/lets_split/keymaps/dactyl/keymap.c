a896c719c3ea1aaaff42465a50a718e5ec294214
Stone Tickle <lattis@mochiro.moe>
2018-12-10 14:13:02 -0600

    add more layers

--- keyboards/lets_split/keymaps/dactyl/keymap.c
+++ keyboards/lets_split/keymaps/dactyl/keymap.c
@@ -5,6 +5,8 @@
 extern keymap_config_t keymap_config;
 
 #define _BASE 0
+#define _VIM  1
+#define _FUNC 2
 
 qk_tap_dance_action_t tap_dance_actions[] = {};
 
@@ -26,8 +28,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_RALT,            KC_K,        KC_N,        KC_E,      KC_I,     KC_O,     KC_QUOT,
       KC_RSPC,            KC_M,        KC_H,        KC_COMM,   KC_DOT,   KC_SLASH, KC_BSLS,
       KC_BSPC,   KC_SPC,               KC_DOWN,     KC_UP,     KC_QUES,  KC_QUES,  KC_QUES
+),
 
-)};
+[_FUNC] = LAYOUT(
+      KC_MINS,   KC_0,    KC_8,        KC_6,        KC_4,      KC_2,               KC_LGUI,
+      KC_LBRC,   KC_Q,    KC_W,        KC_F,        KC_P,      KC_B,               KC_LCTL,
+      KC_GRV,    KC_A,    KC_R,        KC_S,        KC_T,      KC_G,               KC_QUES,
+      KC_QUES,   KC_Z,    KC_X,        KC_C,        KC_D,      KC_V,               KC_LSPO,
+      KC_QUES,   KC_QUES, KC_QUES,     KC_LEFT,     KC_RIGHT,            KC_ENTER, KC_TAB,
+
+      KC_QUES,            KC_1,        KC_3,        KC_5,      KC_7,     KC_9,     KC_EQL,
+      KC_ESC,             KC_J,        KC_L,        KC_U,      KC_Y,     KC_SCLN,  KC_RBRC,
+      KC_RALT,            KC_K,        KC_N,        KC_E,      KC_I,     KC_O,     KC_QUOT,
+      KC_RSPC,            KC_M,        KC_H,        KC_COMM,   KC_DOT,   KC_SLASH, KC_BSLS,
+      KC_BSPC,   KC_SPC,               KC_DOWN,     KC_UP,     KC_QUES,  KC_QUES,  KC_QUES
+)
+
+[_VIM] = LAYOUT(
+      KC_MINS,   KC_0,    KC_8,        KC_6,        KC_4,      KC_2,               KC_LGUI,
+      KC_LBRC,   KC_Q,    KC_W,        KC_F,        KC_P,      KC_B,               KC_LCTL,
+      KC_GRV,    KC_A,    KC_R,        KC_S,        KC_T,      KC_G,               KC_QUES,
+      KC_QUES,   KC_Z,    KC_X,        KC_C,        KC_D,      KC_V,               KC_LSPO,
+      KC_QUES,   KC_QUES, KC_QUES,     KC_LEFT,     KC_RIGHT,            KC_ENTER, KC_TAB,
+
+      KC_QUES,            KC_1,        KC_3,        KC_5,      KC_7,     KC_9,     KC_EQL,
+      KC_ESC,             KC_J,        KC_L,        KC_U,      KC_Y,     KC_SCLN,  KC_RBRC,
+      KC_RALT,            KC_K,        KC_N,        KC_E,      KC_I,     KC_O,     KC_QUOT,
+      KC_RSPC,            KC_M,        KC_H,        KC_COMM,   KC_DOT,   KC_SLASH, KC_BSLS,
+      KC_BSPC,   KC_SPC,               KC_DOWN,     KC_UP,     KC_QUES,  KC_QUES,  KC_QUES
+)
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
