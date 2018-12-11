b0bfb96948c0d4f23fa6539d80c50df4aa1367e0
Stone Tickle <lattis@mochiro.moe>
2018-12-11 10:34:08 -0600

    replace some keys with more functional ones

--- keyboards/lets_split/keymaps/dactyl/keymap.c
+++ keyboards/lets_split/keymaps/dactyl/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT(
       KC_MINS,  KC_0,     KC_8,     KC_6,     KC_4,     KC_2,               KC_LGUI,
       KC_LBRC,  KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,               KC_LCTL,
-      KC_GRV,   KC_A,     KC_R,     KC_S,     KC_T,     KC_G,               KC_QUES,
+      KC_GRV,   KC_A,     KC_R,     KC_S,     KC_T,     KC_G,               KC_LALT,
       KC_QUES,  KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,               KC_LSPO,
       KC_QUES,  MO(_FUNC),MO(_NAVI),KC_LEFT,  KC_RIGHT,           KC_TAB,   KC_ENTER,
 
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC,             KC_J,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_RBRC,
       KC_RALT,            KC_K,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,
       KC_RSPC,            KC_M,     KC_H,     KC_COMM,  KC_DOT,   KC_SLASH, KC_BSLS,
-      KC_SPC,   KC_BSPC,            KC_DOWN,  KC_UP,    KC_QUES,  KC_QUES,  KC_QUES
+      KC_SPC,   KC_BSPC,            KC_DOWN,  KC_UP,    KC_RGUI,  KC_QUES,  KC_QUES
 ),
 [_FUNC] = LAYOUT(
       _______,  _______,  KC_F8,    KC_F6,    KC_F4,    KC_F2,             _______,
